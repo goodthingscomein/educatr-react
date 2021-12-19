1a3301d103d59ab38a73308a97c10dcb24f5adf6
goodthingscomein <31185022+goodthingscomein@users.noreply.github.com>
2021-12-19 15:34:26 +1100

    Setup Create React App with Typescript, created folder structure, eslint, npm installed redux, socket io, material ui

---
+++ src/components/hello-world/hello-world.components.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Import Material UI Components
+import { Button } from '@mui/material';
+
+const HelloWorld: React.FC = () => {
+  return <Button variant='contained'>Hello World!</Button>;
+};
+
+export default HelloWorld;
