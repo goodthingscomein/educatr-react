41f4d0aa546add7d89b4f8f34d4ac46210406586
goodthingscomein <jaicarey98@gmail.com>
2022-02-01 17:42:38 +1100

    cleaned up packages, can specify port to open on

--- src/components/form-input/form-input.components.tsx
+++ src/components/form-input/form-input.components.tsx
@@ -1,72 +1,74 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
 
 // Import styles
 import { InputFieldContainer, InputField, InputFieldLabel, PasswordHiddenToggleButton } from './form-input.styles';
 
+// Import custom icons
+import VisibleIcon from '@mui/icons-material/VisibilityOutlined';
+import HiddenIcon from '@mui/icons-material/VisibilityOffOutlined';
+
 // Used to identify fields for error detection
 export enum INPUT_FIELD_ERROR_ID {
-	EMAIL,
-	PASSWORD,
-	CONFIRM_PASSWORD,
+  EMAIL,
+  PASSWORD,
+  CONFIRM_PASSWORD,
 }
 export type InputErrorMessage = {
-	fieldId: INPUT_FIELD_ERROR_ID;
-	message: string;
+  fieldId: INPUT_FIELD_ERROR_ID;
+  message: string;
 };
 
 // Component Props Interface
 type Props = {
-	type: React.HTMLInputTypeAttribute;
-	label: string;
-	inputId: string;
-	hasPassword?: boolean;
-	value: string | number;
-	onChangeStateDispatch: React.Dispatch<React.SetStateAction<any>>;
+  type: React.HTMLInputTypeAttribute;
+  label: string;
+  inputId: string;
+  hasPassword?: boolean;
+  value: string | number;
+  onChangeStateDispatch: React.Dispatch<React.SetStateAction<any>>;
 };
 
 // Render Component
 const FormInput: React.FC<Props> = ({ type, label, inputId, value, onChangeStateDispatch, hasPassword }) => {
-	// Password hidden states
-	const [passwordHidden, setPasswordHidden] = useState(true);
-	const [inputFieldType, setInputFieldType] = useState(type);
+  // Password hidden states
+  const [passwordHidden, setPasswordHidden] = useState(true);
+  const [inputFieldType, setInputFieldType] = useState(type);
 
-	// Toggle password hidden
-	const togglePasswordHidden = () => {
-		setPasswordHidden(!passwordHidden);
-		passwordHidden ? setInputFieldType('text') : setInputFieldType('password');
-	};
+  // Toggle password hidden
+  const togglePasswordHidden = () => {
+    setPasswordHidden(!passwordHidden);
+    passwordHidden ? setInputFieldType('text') : setInputFieldType('password');
+  };
 
-	// Handle change of an input field
-	const handleChange = (
-		event: React.ChangeEvent<HTMLInputElement>,
-		stateDispatch: React.Dispatch<React.SetStateAction<any>>
-	) => {
-		// Set the value of the input using the state dispatch we passed
-		stateDispatch(event.target.value);
-	};
+  // Handle change of an input field
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    stateDispatch: React.Dispatch<React.SetStateAction<any>>
+  ) => {
+    // Set the value of the input using the state dispatch we passed
+    stateDispatch(event.target.value);
+  };
 
-	return (
-		<InputFieldContainer>
-			<InputFieldLabel htmlFor={inputId}>{label}</InputFieldLabel>
-			<InputField
-				value={value}
-				type={inputFieldType}
-				name={inputId}
-				id={inputId}
-				onChange={(e) => handleChange(e, onChangeStateDispatch)}
-				autoComplete='off'
-			/>
-			{hasPassword ? (
-				<PasswordHiddenToggleButton onClick={() => togglePasswordHidden()}>
-					{passwordHidden ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
-				</PasswordHiddenToggleButton>
-			) : (
-				''
-			)}
-		</InputFieldContainer>
-	);
+  return (
+    <InputFieldContainer>
+      <InputFieldLabel htmlFor={inputId}>{label}</InputFieldLabel>
+      <InputField
+        value={value}
+        type={inputFieldType}
+        name={inputId}
+        id={inputId}
+        onChange={(e) => handleChange(e, onChangeStateDispatch)}
+        autoComplete='off'
+      />
+      {hasPassword ? (
+        <PasswordHiddenToggleButton onClick={() => togglePasswordHidden()}>
+          {passwordHidden ? <HiddenIcon fontSize='large' /> : <VisibleIcon fontSize='large' />}
+        </PasswordHiddenToggleButton>
+      ) : (
+        ''
+      )}
+    </InputFieldContainer>
+  );
 };
 
 export default FormInput;
